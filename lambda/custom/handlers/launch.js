9fb2f2b8d29316e8f89bb393ef18101d1532af2e
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2018-11-15 20:30:18 +0100

    using SSML to speak italian word

--- lambda/custom/handlers/launch.js
+++ lambda/custom/handlers/launch.js
@@ -2,7 +2,10 @@ const { isType } = require('../utils/requests')
 
 const canHandle = handlerInput => isType(handlerInput, 'LaunchRequest')
 const handle = handlerInput => {
-  const speechText = 'Welcome to the Alexa Ideato Workshop, I hope that you will enjoy this day with me'
+  const speechText = `<speak>
+    <p>Welcome to the Alexa <lang xml:lang="it-IT">Ideato</lang> Workshop</p> 
+    <p>I hope that you will enjoy this day with me</p>
+  </speak>`
 
   return handlerInput.responseBuilder
     .speak(speechText)
