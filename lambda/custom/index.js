3b71ffe34a07dcaaf232cdcc95d811b994856dfc
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2018-11-19 18:22:54 +0100

    Added bespoken integration

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,5 +1,7 @@
 const Alexa = require('ask-sdk-core')
 const translate = require('./lib/translate')
+const loglessClient = require('logless-client');
+const { BESPOKEN_SECRET } = require('./secrets.json')
 
 const launchRequestHandlerFactory = require('./handlers/launch')
 const errorHandlerFactory = require('./handlers/error')
@@ -8,13 +10,16 @@ const participantsListHandlerFactory = require('./handlers/participantsList')
 
 const skillBuilder = Alexa.SkillBuilders.custom()
 
-exports.handler = skillBuilder
-  .addRequestHandlers(
-    launchRequestHandlerFactory(translate),
-    helpIntentHandlerFactory(translate),
-    participantsListHandlerFactory(translate)
-  )
-  .addErrorHandlers(
-    errorHandlerFactory(translate)
-  )
-  .lambda()
+const lambda = skillBuilder
+.addRequestHandlers(
+  launchRequestHandlerFactory(translate),
+  helpIntentHandlerFactory(translate),
+  participantsListHandlerFactory(translate)
+)
+.addErrorHandlers(
+  errorHandlerFactory(translate)
+)
+.lambda()
+
+
+exports.handler = loglessClient.Logless.capture(BESPOKEN_SECRET, lambda)
